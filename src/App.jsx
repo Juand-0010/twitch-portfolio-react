82ca93537d17b6826793175d3a766017d0cd8625
Juand-0010 <juanviedma9@gmail.com>
2026-01-11 13:01:43 -0500

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main>
+      <h1>🧩🎮 Bienvenido al canal</h1>
+      <p>
+        Aquí armamos rompecabezas 3D pieza por pieza mientras jugamos
+        videojuegos para relajarnos y pasarla bien.
+      </p>
+      <p>🔴 En vivo armando, jugando y compartiendo.</p>
+    </main>
+  );
+}
+
+export default App;
